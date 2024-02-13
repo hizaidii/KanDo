fa239247c0b2caa140f9262eb11d1e8326edf266
hizaidii <90659514+hizaidii@users.noreply.github.com>
2024-02-14 03:13:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const moment = require("moment");
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 // Session middleware configuration
 app.use(
